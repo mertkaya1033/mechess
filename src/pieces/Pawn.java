34ba17e8b0b5b7d9eb81ea7bdab6fdaaf5a08a1d
Mert Kaya <30465847+sertcast@users.noreply.github.com>
2018-12-24 23:28:27 -0500

    added the same properties to the black pawn

--- src/pieces/Pawn.java
+++ src/pieces/Pawn.java
@@ -39,20 +39,20 @@ public class Pawn extends Piece {
 
             //capture
             indexCheck = index[0] - 1 >= 0 && index[1] - 1 >= 0;
-            if(indexCheck && !board[index[0] - 1][index[1] - 1].isPieceNull()) {
+            if (indexCheck && !board[index[0] - 1][index[1] - 1].isPieceNull()) {
                 isSameColor = board[index[0] - 1][index[1] - 1].getPiece().getColor() == this.color;
-            }else{
+            } else {
                 isSameColor = true;
             }
-            if (!isSameColor ) {
+            if (!isSameColor) {
                 threads.add(board[index[0] - 1][index[1] - 1]);
                 board[index[0] - 1][index[1] - 1].addThreat(this);
             }
 
             indexCheck = index[0] - 1 >= 0 && index[1] + 1 <= board.length - 1;
-            if(indexCheck && !board[index[0] - 1][index[1] + 1].isPieceNull()) {
+            if (indexCheck && !board[index[0] - 1][index[1] + 1].isPieceNull()) {
                 isSameColor = board[index[0] - 1][index[1] + 1].getPiece().getColor() == this.color;
-            }else{
+            } else {
                 isSameColor = true;
             }
             if (!isSameColor) {
@@ -61,32 +61,44 @@ public class Pawn extends Piece {
             }
 
         } else {
-//            if (board.getBoard()[index[0]][index[1] + 1].isPieceNull()) {
-//                //moving one square forward
-//                //add index out of bounds check
-//                threads.add(board.getBoard()[index[0]][index[1] + 1]);
-//                board.getBoard()[index[0]][index[1] + 1].addThreat(this);
-//
-//                //moving two squares forward
-//                //add index out of bounds check
-//                if (firstMove && board.getBoard()[index[0]][index[1] + 2].isPieceNull()) {
-//                    threads.add(board.getBoard()[index[0]][index[1] + 2]);
-//                    board.getBoard()[index[0]][index[1] + 2].addThreat(this);
-//                }
-//            }
-//            //moving capturing a piece
-//            //add index out of bounds check
-//            if (!board.getBoard()[index[0] + 1][index[1] + 1].isPieceNull()) {
-//                threads.add(board.getBoard()[index[0] + 1][index[1] + 1]);
-//                board.getBoard()[index[0] + 1][index[1] - 1].addThreat(this);
-//            }
-//
-//            //moving capturing a piece
-//            //add index out of bounds check
-//            if (!board.getBoard()[index[0] - 1][index[1] + 1].isPieceNull()) {
-//                threads.add(board.getBoard()[index[0] - 1][index[1] + 1]);
-//                board.getBoard()[index[0] - 1][index[1] - 1].addThreat(this);
-//            }
+            //movement
+            //one move ahead check
+            indexCheck = index[0] + 1 <= board.length - 1;
+            if (indexCheck && board[index[0] + 1][index[1]].isPieceNull()) {
+
+                threads.add(board[index[0] + 1][index[1]]);
+                board[index[0] + 1][index[1]].addThreat(this);
+                //two move ahead check
+                indexCheck = index[0] + 2 <= board.length - 1;
+                System.out.println(indexCheck);
+                if (firstMove && indexCheck && board[index[0] + 2][index[1]].isPieceNull()) {
+                    threads.add(board[index[0] + 2][index[1]]);
+                    board[index[0] + 2][index[1]].addThreat(this);
+                }
+            }
+
+            //capture
+            indexCheck = index[0] + 1 <= board.length - 1 && index[1] - 1 >= 0;
+            if (indexCheck && !board[index[0] + 1][index[1] - 1].isPieceNull()) {
+                isSameColor = board[index[0] + 1][index[1] - 1].getPiece().getColor() == this.color;
+            } else {
+                isSameColor = true;
+            }
+            if (!isSameColor) {
+                threads.add(board[index[0] + 1][index[1] - 1]);
+                board[index[0] + 1][index[1] - 1].addThreat(this);
+            }
+
+            indexCheck = index[0] + 1 <= board.length - 1 && index[1] + 1 <= board.length - 1;
+            if (indexCheck && !board[index[0] + 1][index[1] + 1].isPieceNull()) {
+                isSameColor = board[index[0] + 1][index[1] + 1].getPiece().getColor() == this.color;
+            } else {
+                isSameColor = true;
+            }
+            if (!isSameColor) {
+                threads.add(board[index[0] + 1][index[1] + 1]);
+                board[index[0] + 1][index[1] + 1].addThreat(this);
+            }
 
         }
     }
